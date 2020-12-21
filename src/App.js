1dc0033e717a07dba31746e54c2a820dea07a511
Sergio Villagomez <43839357+smavo@users.noreply.github.com>
2020-12-21 11:20:40 -0500

    refactor: Primer Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <>
+      <h2>Frases de Breaking Bad en React 16.13.1</h2>
+    </>
+  );
+}
+
+export default App;
